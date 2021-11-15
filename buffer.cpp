44ba9d559e01f12679ff26e02e195dc6d0738461
Jake Tremblay <tremblay.jake@outlook.com>
2021-11-15 13:02:07 -0800

    Add files via upload

--- buffer.cpp
+++ buffer.cpp
@@ -3,55 +3,55 @@
 //buffer needs to be circular 
 buffer_item buffer[BUFFER_SIZE];
 
-int fpos, rpos; 
-fpos = -1; 
+int fpos, rpos;
+fpos = -1;
 rpos = -1;
 
 
-int insert_item(buffer_item item) { 
-/*
-	* insert
-	* return 0 if success
-	* return -1 if fail
-	*/
-	  if ((fpos == 0 && rpos == BUFFER_SIZE-1) ||
-            (rpos == (fpos-1)%(BUFFER_SIZE-1)))
+int insert_item(buffer_item item) {
+    /*
+        * insert
+        * return 0 if success
+        * return -1 if fail
+        */
+    if ((fpos == 0 && rpos == BUFFER_SIZE - 1) ||
+        (rpos == (fpos - 1) % (BUFFER_SIZE - 1)))
     {
-        cout<<"Buffer is Full"<<endl;
+        cout << "Buffer is Full" << endl;
         return -1;
     }
- 
+
     else if (fpos == -1) /* Insert First Element */
     {
         fpos = rpos = 0;
         buffer[rpos] = item;
     }
- 
-    else if (rpos == BUFFER_SIZE-1 && fpos != 0)
+
+    else if (rpos == BUFFER_SIZE - 1 && fpos != 0)
     {
         rpos = 0;
         buffer[rpos] = item;
     }
- 
+
     else
     {
         rpos++;
         buffer[rpos] = item;
     }
-	return 0;
+    return 0;
 }
 
 int remove_item(buffer_item* item) {
-	/*remove item
-	* return 0 if success
-	* return -1 if  fail 
-	*/
-	 if (fpos == -1)
+    /*remove item
+    * return 0 if success
+    * return -1 if  fail
+    */
+    if (fpos == -1)
     {
-        cout<<"Buffer is Empty"<<endl;
+        cout << "Buffer is Empty" << endl;
         return -1;
     }
- 
+
     int item = buffer[fpos];
     buffer[fpos] = -1;
     if (fpos == rpos)
@@ -59,35 +59,35 @@ int remove_item(buffer_item* item) {
         fpos = -1;
         rpos = -1;
     }
-    else if (fpos == BUFFER_SIZE-1)
+    else if (fpos == BUFFER_SIZE - 1)
         fpos = 0;
     else
         fpos++;
- 
+
     return item;
-	return 0;
+    return 0;
 }
 
 void display_buffer()
 {
     if (fpos == -1)
     {
-        cout<<"Buffer is Empty"<<endl;
+        cout << "Buffer is Empty" << endl;
         return;
     }
-    cout<<"The current content of the buffer is [";
+    cout << "The current content of the buffer is [";
     if (rpos >= fpos)
     {
         for (int i = fpos; i <= rpos; i++)
-            cout<<buffer[i]<<",";
+            cout << buffer[i] << ",";
     }
     else
     {
         for (int i = fpos; i < BUFFER_SIZE; i++)
-            cout<<buffer[i]<<",";
- 
+            cout << buffer[i] << ",";
+
         for (int i = 0; i <= rpos; i++)
-            cout<<buffer[i]<<",";
+            cout << buffer[i] << ",";
     }
-	cout<<"]"<<endl;
+    cout << "]" << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,43 +1,62 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <time.h>
 #include "buffer.h"
 
 int insert_item(buffer_item item);
 int remove_item(buffer_item* item);
+sem_t emptySem, fullSem;
+pthread_mutex_t mutex;
 
-
+/*Producer Thread*/
 void* producer(void* param) {
 	buffer_item item;
 
 	while (true) {
 		//sleep for random time
-		//sleep(...);
+		sleep(rand() % 10);
 		//generate random number
-		item = rand();
-		if (insert_item(item))
+		item = rand() % 10000000;;
+
+		sem_wait(sem);		//aquire the semaphore
+
+		if (insert_item(item))								//critical section
 			std::cout << "report error condition" << '\n';
 		else
 			std::cout << "producer produced item: " << item << '\n';
+
+		sem_post(sem);		//release the semaphore
+
 	}
 }
 
+/*Consumer Thread*/
 void* consumer(void* param) {
 	buffer_item item;
 
 	while (true) {
 		//sleep for random time
-		//sleep(...);
+		sleep(rand() % 10);
 		//generate random number
-		item = rand();
+		item = rand() % 10000000;
+
+		sem_wait(sem);		//aquire the semaphore
+
 		if (remove_item(&item))
-			std::cout << "report error condition" << '\n';
+			std::cout << "report error condition" << '\n';			//critical section
 		else
 			std::cout << "consumer consumed item: " << item << '\n';
+
+		sem_post(sem);		//release the semaphore
 	}
 }
 
 int main(int argc, char* argv[]) {
+	
+	srand(time(NULL));
+	
 	//  1. Get CLI argv[1] argv[2] argv[3] -> sleep time, number of producers, number of consumers
 
 	//--- should probably do error checking on the input ---
@@ -45,7 +64,13 @@ int main(int argc, char* argv[]) {
 	int producerCount = atoi(argv[2]);
 	int consumerCount = atoi(argv[3]);
 	
-	//	2. Init buffers
+	//	2. Init semaphores, mutex, buffers
+	sem_init(&emptySem, 0, 0);
+	sem_init(&fullSem, 0, 0);
+	
+
+	pthread_mutex_init(&mutex, NULL);
+
 	pthread_t producers[producerCount];
 	pthread_t consumers[consumerCount];
 	
@@ -53,16 +78,16 @@ int main(int argc, char* argv[]) {
 	//	3. Create producer threads
 	for (int i = 0; i < producerCount; i++) {
 		//create producer
-		//pthread_create()
+		pthread_create(&producers, NULL, producer, NULL);		
 	}
 
 	//	4. Create consumer threads
 	for (int i = 0; i < consumerCount; i++) {
 		//create consumers
-		//pthread_create()
+		pthread_create(&consumers, NULL, consumer, NULL);		
 	}
 	//	5. Sleep
-	//sleep(sleepTime);
+	sleep(sleepTime);
 	
 	//	6. Exit
 	return 0;
